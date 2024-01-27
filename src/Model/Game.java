d1127fea186cb928a7f5fcf93fb4be66e4939d8a
aseelsamry <samry123aseel@gmail.com>
2024-01-27 19:20:55 +0200

    Update Game.java

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -1,10 +1,12 @@
 package Model;
 
+import java.time.LocalDateTime;
+
 public class Game {
 	private int gameID;
 	private DIFFICULTY difficultyGame;
 	private Board boardgame;
 	private Player winningPlayer;
-	
+	private LocalDateTime timer;
 
 }
