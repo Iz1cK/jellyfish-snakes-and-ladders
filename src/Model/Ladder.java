7c51904d02598c5fbcc33be96e29663dc0df66e4
lanamukh98 <lanamukh98@gmail.com>
2024-01-28 21:41:32 +0200

    function updated
    setStartAndDest() in Ladders class have been updated

--- src/Model/Ladder.java
+++ src/Model/Ladder.java
@@ -51,9 +51,18 @@ public class Ladder {
 
 		switch (diffLevel) {
         case EASY:
-        	Square easyS1= new Square(random.nextInt(10 - 1 + 1));
-        	Square easyS2= new Square(random.nextInt(40 - 11 + 1));
-        	Ladder ladderE1= new Ladder ("easy1",easyS1,easyS2);
+        	Square easySR1= new Square(random.nextInt(10 - 1 + 1));
+        	Square easySC1= new Square(random.nextInt(20 - 11 + 1));
+        	Ladder ladderE1= new Ladder ("easy1",easySR1,easySC1);
+        	Square easySR2= new Square(random.nextInt(20 - 1 + 1));
+        	Square easySC2= new Square(random.nextInt(40 - 11 + 1));
+        	Ladder ladderE2= new Ladder ("easy2",easySR2,easySC2);
+        	Square easySR3= new Square(random.nextInt(40 - 1 + 1));
+        	Square easySC3= new Square(random.nextInt(70 - 11 + 1));
+        	Ladder ladderE3= new Ladder ("easy3",easySR3,easySC3);
+        	Square easySR4= new Square(random.nextInt(40 - 1 + 1));
+        	Square easySC4= new Square(random.nextInt(80 - 11 + 1));
+        	Ladder ladderE4= new Ladder ("easy4",easySR4,easySC4);
             System.out.println("HARD");
             break;
         case MEDIUM:
