43ca762d915e24267a4ce07cfb93f454f83586a8
lanamukh98 <lanamukh98@gmail.com>
2024-02-04 20:29:18 +0200

    Update Snake.java
    function updated

--- src/Model/Snake.java
+++ src/Model/Snake.java
@@ -18,27 +18,91 @@ public class Snake {
 		
 	}
 	
-	public List<Snake> setStartAndDest( int gridSize, int numberOfSnakes) {
+	public Snake() {
+		super();
+	}
+	
+	public void setStartAndDest( ArrayList<Integer> questions, ArrayList<Integer> surprise, int numberOfSnakes, int boardSize) {
 		Random random = new Random();
-		List<Snake> snakes = new ArrayList<>();
+		ArrayList<Snake> snakes = new ArrayList<>();
 		
-		// Create List for all board's squares
-        List<Square> allSquares = new ArrayList<>();
-        for (int i = 1; i <= gridSize * gridSize; i++) {
-            allSquares.add(new Square(i));
-        }
-
-        for (int i = 1; i <= numberOfSnakes; i++) {
-            //initial start square for the ladder
-            Square snakeEnd = allSquares.remove(random.nextInt(allSquares.size()+(i+1)));
-
-            Square snakeStart = allSquares.remove(random.nextInt((snakeEnd.getPosition()%i-i*gridSize)+(gridSize)));
-            //adding the ladder to the ladders array
-            snakes.add(new Snake(String.valueOf(i),snakeStart, snakeEnd));
-        }
-        return snakes;
-
+		int snakeStart;
+		for(int i=1;i <= numberOfSnakes; i++) {
+		snakeStart = getRandom(boardSize + 1 + (i-1), boardSize*boardSize + 1);
+		while(questions.contains(snakeStart) || surprise.contains(snakeStart))
+			snakeStart = getRandom(boardSize + 1 + (i-1)*boardSize, boardSize*boardSize + 1);
+			// SNAKE HEAD
+			System.out.println("Snake Start: " + snakeStart);
+			headSquare = new Square(snakeStart);
+			// SNAKE TAIL
+			int row = (int) Math.floor(snakeStart/boardSize);
+			int snakeEnd = getRandom(boardSize*(row-i) + 1, boardSize*row + (i-1));
+			System.out.println("Snake End: "  + snakeEnd);
+			tailSquare = new Square(snakeEnd);
+		}
+            
     }
+	
+	public String getSnakeID() {
+		return snakeID;
+	}
+
+	public void setSnakeID(String snakeID) {
+		this.snakeID = snakeID;
+	}
+
+	public COLORS getSnakeSnake() {
+		return snakeSnake;
+	}
+
+	public void setSnakeSnake(COLORS snakeSnake) {
+		this.snakeSnake = snakeSnake;
+	}
+
+	public Square getHeadSquare() {
+		return headSquare;
+	}
+
+	public void setHeadSquare(Square headSquare) {
+		this.headSquare = headSquare;
+	}
+
+	public Square getTailSquare() {
+		return tailSquare;
+	}
+
+	public void setTailSquare(Square tailSquare) {
+		this.tailSquare = tailSquare;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	@Override
+	public String toString() {
+		return "Snake [snakeID=" + snakeID + ", snakeSnake=" + snakeSnake + ", headSquare=" + headSquare
+				+ ", tailSquare=" + tailSquare + ", rank=" + rank + "]";
+	}
+
+	private static int getRandom(int from, int to) {
+	    if (from < to)
+	        return from + new Random().nextInt(Math.abs(to - from));
+	    return from - new Random().nextInt(Math.abs(to - from));
+	}
+	// for testing the methods
+	public static void main(String[] args) {
+		Snake snake = new Snake();
+		ArrayList<Integer> questions = new ArrayList<>();
+		questions.add(10);
+		ArrayList<Integer> surprise = new ArrayList<>();
+		surprise.add(16);
+		snake.setStartAndDest(questions,surprise,4,10);
+	}
 	
 
 }
