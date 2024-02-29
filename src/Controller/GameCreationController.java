1cf6882b4ae62c220bddb1fe668cc0067145dae8
lanamukh98 <lanamukh98@gmail.com>
2024-02-29 23:05:01 +0200

    Update GameCreationController.java

--- src/Controller/GameCreationController.java
+++ src/Controller/GameCreationController.java
@@ -1,6 +1,32 @@
 package Controller;
 
+import java.util.ArrayList;
+
+import Model.DIFFICULTY;
+import Model.Player;
+
 public class GameCreationController {
 	
+	DIFFICULTY difficultyLevel;
+	public void setDifficultyLevel(DIFFICULTY difficulty) {
+        this.difficultyLevel = difficulty;
+    }
+	
+	ArrayList<Player> players=new ArrayList<Player>();
+	
+	public ArrayList<Player> getPlayers() {
+		return players;
+	}
 
+	public void setPlayers(ArrayList<Player> players) {
+		this.players = players;
+	}
+
+	@Override
+	public String toString() {
+		return "GameCreationController [difficultyLevel=" + difficultyLevel + ", players=" + players + "]";
+	}
+	
+	
+	
 }
