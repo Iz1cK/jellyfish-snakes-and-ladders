c0ad37ec897cfd8d24b5979ce4e91017e9725f84
aseelsamry <samry123aseel@gmail.com>
2024-03-01 09:50:17 +0200

    question popup and tested it

---
+++ src/Controller/questionPopUpController.java
@@ -0,0 +1,118 @@
+package Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import Model.Questions;
+import Model.Sysdata;
+import View.QuestionPopUp;
+import View.Test;
+
+public class questionPopUpController {
+	Sysdata sysdata= Sysdata.getInstance();
+	Test test;
+	String question;
+	String answer1;
+	String answer2;
+	String answer3;
+	String answer4;
+	public List<Questions> Questions= new ArrayList<>();
+	Random random = new Random();
+	private static questionPopUpController instance;
+	public static questionPopUpController getInstance() {
+		if (instance == null)
+			instance = new questionPopUpController();
+		return instance;
+	}
+	
+	public void questionRank(int diffculty) {
+		for (Questions qd : sysdata.questionsList) {
+			if(qd.getDifficulty()==diffculty) {
+				Questions.add(qd);
+			}
+		}
+		if (!Questions.isEmpty()) {
+		    // Generate a random index within the bounds of the list's size
+		    int randomIndex = random.nextInt(Questions.size());
+
+		    // Retrieve the question at the random index
+		    Questions randomQuestion = Questions.get(randomIndex);
+		    question= randomQuestion.getQuestion();
+		    answer1= randomQuestion.getAnswers().get(0);
+			answer2= randomQuestion.getAnswers().get(1);
+			answer3= randomQuestion.getAnswers().get(2);
+			answer4= randomQuestion.getAnswers().get(3);
+		    // Now you have a random question from the easyQuestion list
+		    // Do something with the random question
+			
+			QuestionPopUp obj = new QuestionPopUp();
+	        obj.cmdobt1.setText(answer1);
+	        obj.cmdobt2.setText(answer2);
+	        obj.cmdobt3.setText(answer3);
+	        obj.cmdobt4.setText(answer4);
+	        String questionBody=question;
+	        obj.correctAns=randomQuestion.getCorrect_ans();
+	        
+	        if(diffculty==1) {
+	        obj.showMessage("easy", questionBody);
+	        }
+	        else if(diffculty==2) {
+	        	obj.showMessage("medium", questionBody);	
+	        }
+	        else if(diffculty==3) {
+	        	obj.showMessage("hard", questionBody);	
+	        }
+	        
+		} else {
+		    // Handle the case where the easyQuestion list is empty
+		    System.out.println("No questions available in the easyQuestion list.");
+		}	
+		
+		
+	}
+	
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getAnswer1() {
+		return answer1;
+	}
+
+	public void setAnswer1(String answer1) {
+		this.answer1 = answer1;
+	}
+
+	public String getAnswer2() {
+		return answer2;
+	}
+
+	public void setAnswer2(String answer2) {
+		this.answer2 = answer2;
+	}
+
+	public String getAnswer3() {
+		return answer3;
+	}
+
+	public void setAnswer3(String answer3) {
+		this.answer3 = answer3;
+	}
+
+	public String getAnswer4() {
+		return answer4;
+	}
+
+	public void setAnswer4(String answer4) {
+		this.answer4 = answer4;
+	}
+
+
+	
+
+}
---
+++ src/View/MessageDialog.java
@@ -0,0 +1,351 @@
+package View;
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import org.jdesktop.animation.timing.Animator;
+import org.jdesktop.animation.timing.TimingTargetAdapter;
+
+public class MessageDialog extends JDialog {
+
+    private final JFrame fram;
+    private Animator animator;
+    private Glass glass;
+    private boolean show;
+    private MessageType messageType = MessageType.CANCEL;
+
+    public MessageDialog() {
+        super();
+        this.fram = new JFrame();
+        initComponents();
+        init();
+    }
+
+    private void init() {
+    	setBackground(new Color(0, 0, 0, 0));
+        StyledDocument doc = txt.getStyledDocument();
+        SimpleAttributeSet center = new SimpleAttributeSet();
+        StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+        doc.setParagraphAttributes(0, doc.getLength(), center, false);
+        txt.setOpaque(false);
+        txt.setBackground(new Color(0, 0, 0, 0));
+        setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                closeMessage();
+            }
+        });
+        animator = new Animator(350, new TimingTargetAdapter() {
+            @Override
+            public void timingEvent(float fraction) {
+                float f = show ? fraction : 1f - fraction;
+                glass.setAlpha(f - f * 0.3f);
+                setOpacity(f);
+            }
+
+            @Override
+            public void end() {
+                if (show == false) {
+                    dispose();
+                    glass.setVisible(false);
+                }
+            }
+        });
+        animator.setResolution(0);
+        animator.setAcceleration(.5f);
+        animator.setDeceleration(.5f);
+        setOpacity(0f);
+        glass = new Glass();
+    }
+
+    private void startAnimator(boolean show) {
+        if (animator.isRunning()) {
+            float f = animator.getTimingFraction();
+            animator.stop();
+            animator.setStartFraction(1f - f);
+        } else {
+            animator.setStartFraction(0f);
+        }
+        this.show = show;
+        animator.start();
+    }
+
+    public void showMessage(String title, String message) {
+        fram.setGlassPane(glass);
+        glass.setVisible(true);
+        lbTitle.setText(title);
+        txt.setText(message);
+        setLocationRelativeTo(fram);
+        startAnimator(true);
+        setVisible(true);
+    }
+
+    public void closeMessage() {
+        startAnimator(false);
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+    	System.out.println("iam in initComponent");
+
+        background1 = new Background();
+        cmdOK = new ButtonCustom();
+        lbIcon = new JLabel();
+        lbTitle = new JLabel();
+        txt = new JTextPane();
+
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setUndecorated(true);
+
+        background1.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
+
+        
+
+        cmdOK.setText("OK");
+        cmdOK.setFont(new Font("sansserif", 0, 14)); // NOI18N
+        cmdOK.setPreferredSize(new Dimension(100, 50)); 
+        cmdOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                cmdOKActionPerformed(evt);
+            }
+        });
+
+        lbIcon.setHorizontalAlignment(SwingConstants.CENTER);
+        lbIcon.setIcon(new ImageIcon(getClass().getResource("/img/icon.png"))); // NOI18N
+
+        lbTitle.setFont(new Font("sansserif", 1, 18)); // NOI18N
+        lbTitle.setForeground(new Color(245, 71, 71));
+        lbTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        lbTitle.setText("Message Title");
+
+        txt.setEditable(false);
+        txt.setFont(new Font("sansserif", 0, 14)); // NOI18N
+        txt.setForeground(new Color(76, 76, 76));
+        txt.setText("Message Text\nSimple");
+        txt.setFocusable(false);
+
+        javax.swing.GroupLayout background1Layout = new javax.swing.GroupLayout(background1);
+        background1.setLayout(background1Layout);
+        background1Layout.setHorizontalGroup(
+            background1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(background1Layout.createSequentialGroup()
+                
+                .addComponent(cmdOK, javax.swing.GroupLayout.DEFAULT_SIZE, 196, Short.MAX_VALUE))
+            .addComponent(lbIcon, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(lbTitle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(txt)
+        );
+        background1Layout.setVerticalGroup(
+            background1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, background1Layout.createSequentialGroup()
+                .addComponent(lbIcon, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20)
+                .addComponent(lbTitle)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(txt, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(background1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cmdOK, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(background1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(background1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        System.out.println("iam in end initComponent");
+    }// </editor-fold>//GEN-END:initComponents
+
+   
+    private void cmdOKActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdOKActionPerformed
+        messageType = MessageType.OK;
+        closeMessage();
+    }//GEN-LAST:event_cmdOKActionPerformed
+
+    public static enum MessageType {
+        CANCEL, OK
+    }
+    
+    class Background extends JPanel {
+
+        public Background() {
+            init();
+        }
+
+        private void init() {
+            setOpaque(false);
+            setBackground(new Color(245, 245, 245));
+            setPreferredSize(new Dimension(700, 400)); 
+            setForeground(new Color(118, 118, 118));
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            int x = 0;
+            int y = 40;
+            int width = getWidth();
+            int height = getHeight();
+            int iconSpace = 7;
+            int totalIconSpace = iconSpace * 2;
+            int iconSize = y * 2;
+            int iconX = (width - (iconSize + totalIconSpace)) / 2;
+            int iconY = 0;
+            Area area = new Area(new Rectangle2D.Double(x, y, width, height - y));
+            area.subtract(new Area(new Ellipse2D.Double(iconX, iconY - iconSpace, iconSize + totalIconSpace, iconSize + totalIconSpace)));
+            area.add(new Area(new Ellipse2D.Double(iconX + iconSpace, 0, iconSize, iconSize)));
+            g2.setColor(getBackground());
+            g2.fill(area);
+            g2.dispose();
+            super.paintComponent(g);
+        }
+    }
+    
+    class Glass extends JComponent {
+
+        public float getAlpha() {
+            return alpha;
+        }
+
+        public void setAlpha(float alpha) {
+            this.alpha = alpha;
+            repaint();
+        }
+
+        public Glass() {
+        }
+
+        private float alpha = 0f;
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+            g2.setColor(Color.gray);
+            g2.fillRect(0, 0, getWidth(), getHeight());
+            g2.dispose();
+            super.paintComponent(g);
+        }
+    }
+    
+    class ButtonCustom extends JButton {
+
+        private Color background = new Color(69, 191, 71);
+        private Color colorHover = new Color(76, 206, 78);
+        private Color colorPressed = new Color(63, 175, 65);
+        private boolean mouseOver = false;
+
+        public ButtonCustom() {
+            init();
+        }
+
+        private void init() {
+            setContentAreaFilled(false);
+            setCursor(new Cursor(Cursor.HAND_CURSOR));
+            setBackground(background);
+            setForeground(Color.WHITE);
+            setOpaque(true);
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    mouseOver = true;
+                    ButtonCustom.super.setBackground(getColorHover());
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    mouseOver = false;
+                    ButtonCustom.super.setBackground(background);
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    ButtonCustom.super.setBackground(getColorPressed());
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                    if (mouseOver) {
+                        ButtonCustom.super.setBackground(getColorHover());
+                    } else {
+                        ButtonCustom.super.setBackground(background);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void setBackground(Color bg) {
+            background = bg;
+            super.setBackground(bg);
+        }
+
+        public Color getColorHover() {
+            return colorHover;
+        }
+
+        public void setColorHover(Color colorHover) {
+            this.colorHover = colorHover;
+        }
+
+        public Color getColorPressed() {
+            return colorPressed;
+        }
+
+        public void setColorPressed(Color colorPressed) {
+            this.colorPressed = colorPressed;
+        }
+    }
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private Background background1;
+    private	ButtonCustom cmdOK;
+    private javax.swing.JLabel lbIcon;
+    private javax.swing.JLabel lbTitle;
+    private javax.swing.JTextPane txt;
+    // End of variables declaration//GEN-END:variables
+}
