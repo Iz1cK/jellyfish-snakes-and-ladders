d86358fa306d6a870590fd056841500006907afc
lanamukh98 <lanamukh98@gmail.com>
2024-03-10 19:21:53 +0200

    adding audio class

--- src/View/AudioTest.java
+++ src/View/AudioTest.java
@@ -6,39 +6,26 @@ import java.io.IOException;
 import java.util.Scanner;
 public class AudioTest {
 
-	 public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException{
+	public static void sounds(String str) throws UnsupportedAudioFileException, IOException, LineUnavailableException{
 		  
-		  Scanner scanner = new Scanner(System.in);
 		  
 		  File file = new File("correctanswer.wav");
 		  AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
 		  Clip clip = AudioSystem.getClip();
 		  clip.open(audioStream);
-		  
-		  String response = "";
 		 
 		  
-		  while(!response.equals("Q")) {
-		   System.out.println("P = play, S = Stop, R = Reset, Q = Quit");
-		   System.out.print("Enter your choice: ");
+		  switch (str) {
+      case "S":
+          clip.start();
+          break;
+      case "P":
+          clip.stop();
+          break;
+      default:
+          System.out.println("Not a valid response");
+  }
 		   
-		   response = scanner.next();
-		   response = response.toUpperCase();
-		   
-		   switch(response) {
-		    case ("P"): clip.start();
-		    break;
-		    case ("S"): clip.stop();
-		    break;
-		    case ("R"): clip.setMicrosecondPosition(0);
-		    break;
-		    case ("Q"): clip.close();
-		    break;
-		    default: System.out.println("Not a valid response");
-		   }
-		   
-		  }
-		  System.out.println("Byeeee!"); 
-		 }
-		}
+}
+}
 
